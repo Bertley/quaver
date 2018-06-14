c9d619a04924b95d76a9ba6ece409b70ce1b251c
Bertley <eedialu@gmail.com>
2018-06-13 23:53:42 -0400

    Added covers to the genre

--- src/app/genre.service.ts
+++ src/app/genre.service.ts
@@ -8,9 +8,9 @@ import {Observable, of} from 'rxjs';
 export class GenreService {
 
   genres: Genre[] = [
-    {id: 1, name: 'Hip-Hop'},
-    {id: 2, name: 'Afro-Beats'},
-    {id: 3, name: 'Electronic/Dance'}
+    {id: 1, name: 'Hip-Hop', cover: 'https://upload.wikimedia.org/wikipedia/commons/9/98/FullSizeRender-3-871x1050.jpg'},
+    {id: 2, name: 'Afro-Beats', cover: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/Lasgiidi_-_Bottles_ft_Olamide.jpg'},
+    {id: 3, name: 'Electronic/Dance', cover: 'https://upload.wikimedia.org/wikipedia/commons/1/14/Luzes-coloridas.jpg'}
   ];
 
   getGenres(): Observable<Genre[]> {
