fa529509a9d498680510be0d3d1cfb0222c01435
Geri Kona <gerikona@hotmail.com>
2018-06-28 22:24:25 -0400

    added links to side nav

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {CommsService} from '../comms.service';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +8,17 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title = 'Quaver';
+  genreSelected = '';
+  artistSelected = '';
+  albumSelected = '';
 
-  constructor() {
+  constructor(private comms: CommsService) {
   }
 
   ngOnInit() {
+    this.comms.genreSelected.subscribe(data => this.genreSelected = data);
+    this.comms.artistSelected.subscribe(data => this.artistSelected = data);
+    this.comms.albumSelected.subscribe(data => this.albumSelected = data);
   }
 
 }
